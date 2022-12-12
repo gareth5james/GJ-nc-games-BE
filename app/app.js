1c66b10938fc4e1b3dda14f04f3630fbca0178e7
gareth5james <gareth5james@yahoo.co.uk>
2022-12-12 15:51:02 +0000

    start get review by ID

--- app/app.js
+++ app/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { badPath } = require("./controllers/errors.js");
 const { getCategories } = require("./controllers/categories.js");
-const { getReviews } = require("./controllers/reviews.js");
+const { getReviews, getReviewById } = require("./controllers/reviews.js");
 
 const app = express();
 
 app.get("/api/categories", getCategories);
 
 app.get("/api/reviews", getReviews);
+app.get("/api/reviews/:review_id", getReviewById);
 
 app.all("*", badPath);
 
