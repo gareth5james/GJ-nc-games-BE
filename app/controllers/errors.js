7746407f6028be0494614debf60f4726a71d685e
gareth5james <gareth5james@yahoo.co.uk>
2022-12-12 16:24:23 +0000

    add custom errors and error for bad data type

--- app/controllers/errors.js
+++ app/controllers/errors.js
@@ -1,3 +1,15 @@
 exports.badPath = (request, response) => {
   response.status(404).send({ message: "Bad path, not found" });
 };
+
+exports.customError = (error, request, response, next) => {
+  if (error.status && error.message) {
+    response.status(error.status).send({ message: error.message });
+  } else next(error);
+};
+
+exports.sqlError = (error, request, response, next) => {
+  if (error.code === "22P02") {
+    response.status(400).send({ message: "Bad data type" });
+  } else next(error);
+};
