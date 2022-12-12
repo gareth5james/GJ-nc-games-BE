0814f29969c8b9731d5181632ea0fe8ddb46854c
gareth5james <gareth5james@yahoo.co.uk>
2022-12-12 16:25:37 +0000

    add controller to app

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { badPath } = require("./controllers/errors.js");
+const { badPath, sqlError, customError } = require("./controllers/errors.js");
 const { getCategories } = require("./controllers/categories.js");
 const { getReviews, getReviewById } = require("./controllers/reviews.js");
 
@@ -11,5 +11,8 @@ app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id", getReviewById);
 
 app.all("*", badPath);
+
+app.use(customError);
+app.use(sqlError);
 
 module.exports = app;
