85e02742dd2a0bed00f9ae6807ccd45620d73c8b
gareth5james <gareth5james@yahoo.co.uk>
2022-12-12 13:57:30 +0000

    remove logs and error interrupts

--- app/models/categories.js
+++ app/models/categories.js
@@ -1,10 +1,8 @@
 const db = require("../../db/connection.js");
 
 exports.selectAllCategories = () => {
-  return db
-    .query(`SELECT * FROM categories;`)
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((error) => console.log(error));
+  return db.query(`SELECT * FROM categories;`).then((result) => {
+    console.log(result.rows);
+    return result.rows;
+  });
 };
