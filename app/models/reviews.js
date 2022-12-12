2385e0677108614a7b483ee806a490084968770e
gareth5james <gareth5james@yahoo.co.uk>
2022-12-12 15:52:11 +0000

    remove logs based on feedback

--- app/models/reviews.js
+++ app/models/reviews.js
@@ -3,8 +3,7 @@ const db = require("../../db/connection.js");
 exports.selectAllReviews = () => {
   return db
     .query(
-      `SELECT owner, title, reviews.review_id, category, review_img_url,
-    reviews.created_at, reviews.votes, designer, 
+      `SELECT reviews.*, 
     COUNT(comment_id) AS comment_count FROM reviews
     LEFT JOIN comments ON comments.review_id = reviews.review_id 
     GROUP BY owner, title, reviews.review_id, category, review_img_url,
