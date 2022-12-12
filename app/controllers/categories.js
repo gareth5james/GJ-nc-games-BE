9caf4f373cd6236ed01ea6a7fa173317979c267c
gareth5james <gareth5james@yahoo.co.uk>
2022-12-12 14:15:49 +0000

    feedback, removed console.logs

--- app/controllers/categories.js
+++ app/controllers/categories.js
@@ -1,7 +1,6 @@
 const { selectAllCategories } = require("../models/categories.js");
 
 exports.getCategories = (request, response, next) => {
-  console.log("got to the controller");
   selectAllCategories()
     .then((categories) => response.send({ categories }))
     .catch(next);
--- app/models/categories.js
+++ app/models/categories.js
@@ -2,7 +2,6 @@ const db = require("../../db/connection.js");
 
 exports.selectAllCategories = () => {
   return db.query(`SELECT * FROM categories;`).then((result) => {
-    console.log(result.rows);
     return result.rows;
   });
 };
