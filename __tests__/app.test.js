28010f9dfd5a764beaa69c7a3ab4b530bf66f244
gareth5james <gareth5james@yahoo.co.uk>
2022-12-13 10:32:25 +0000

    amend test on 5. to objectContaining

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -98,19 +98,21 @@ describe("5. GET /api/reviews/:review_id", () => {
       .get("/api/reviews/12")
       .expect(200)
       .then(({ body: { review } }) => {
-        expect(review).toEqual({
-          review_id: 12,
-          title: "Scythe; you're gonna need a bigger table!",
-          review_body:
-            "Spend 30 minutes just setting up all of the boards (!) meeple and decks, just to forget how to play. Scythe can be a lengthy game but really packs a punch if you put the time in. With beautiful artwork, countless scenarios and clever game mechanics, this board game is a must for any board game fanatic; just make sure you explain ALL the rules before you start playing with first timers or you may find they bring it up again and again.",
-          designer: "Jamey Stegmaier",
-          review_img_url:
-            "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg",
-          votes: 100,
-          category: "social deduction",
-          owner: "mallionaire",
-          created_at: "2021-01-22T10:37:04.839Z",
-        });
+        expect(review).toEqual(
+          expect.objectContaining({
+            review_id: 12,
+            title: "Scythe; you're gonna need a bigger table!",
+            review_body:
+              "Spend 30 minutes just setting up all of the boards (!) meeple and decks, just to forget how to play. Scythe can be a lengthy game but really packs a punch if you put the time in. With beautiful artwork, countless scenarios and clever game mechanics, this board game is a must for any board game fanatic; just make sure you explain ALL the rules before you start playing with first timers or you may find they bring it up again and again.",
+            designer: "Jamey Stegmaier",
+            review_img_url:
+              "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg",
+            votes: 100,
+            category: "social deduction",
+            owner: "mallionaire",
+            created_at: "2021-01-22T10:37:04.839Z",
+          })
+        );
       });
   });
 
@@ -132,3 +134,5 @@ describe("5. GET /api/reviews/:review_id", () => {
       });
   });
 });
+
+describe("6. GET /api/reviews/:review_id/comments", () => {});
