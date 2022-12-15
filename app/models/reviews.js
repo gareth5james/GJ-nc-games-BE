7c61d48d9e6a36d9d21687721ef586726cea9d90
gareth5james <gareth5james@yahoo.co.uk>
2022-12-15 08:52:37 +0000

    update select review by id

--- app/models/reviews.js
+++ app/models/reviews.js
@@ -43,7 +43,13 @@ exports.selectAllReviews = (
 
 exports.selectReviewById = (id) => {
   return db
-    .query(`SELECT * FROM reviews WHERE review_id = $1`, [id])
+    .query(
+      `SELECT reviews.*, COUNT(comment_id) AS comment_count 
+      FROM reviews 
+      LEFT JOIN comments ON comments.review_id = reviews.review_id
+      WHERE reviews.review_id = $1 GROUP BY reviews.review_id`,
+      [id]
+    )
     .then((result) => {
       if (result.rows.length === 0) {
         return Promise.reject({
