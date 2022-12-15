f144c0e0301bf8cddee4a3136149acb477bdca96
gareth5james <gareth5james@yahoo.co.uk>
2022-12-15 15:19:41 +0000

    add categories router

---
+++ app/routers/categoriesRouter.js
@@ -0,0 +1,7 @@
+const categoriesRouter = require("express").Router();
+
+const { getCategories } = require("../controllers/categories");
+
+categoriesRouter.route("/").get(getCategories);
+
+module.exports = categoriesRouter;
