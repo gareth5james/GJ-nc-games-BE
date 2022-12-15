b5e531e5d89e39a20d847a7de046d2acac04a28a
gareth5james <gareth5james@yahoo.co.uk>
2022-12-15 15:21:11 +0000

    use routers

--- app/app.js
+++ app/app.js
@@ -1,43 +1,15 @@
 const express = require("express");
 const { badPath, sqlError, customError } = require("./controllers/errors.js");
-const { getCategories } = require("./controllers/categories.js");
 
-const {
-  getReviews,
-  getReviewById,
-  patchReviewVotesById,
-} = require("./controllers/reviews.js");
-
-const {
-  getCommentsByReviewId,
-  postNewComment,
-  deleteComment,
-} = require("./controllers/comments.js");
-
-const { getApi } = require("./controllers/api.js");
-
-const { getUsers } = require("./controllers/users.js");
+const apiRouter = require("./routers/apiRouter.js");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/api", getApi);
-
-app.get("/api/categories", getCategories);
-
-app.get("/api/reviews", getReviews);
-app.get("/api/reviews/:review_id", getReviewById);
-app.patch("/api/reviews/:review_id", patchReviewVotesById);
-
-app.get("/api/reviews/:review_id/comments", getCommentsByReviewId);
-app.post("/api/reviews/:review_id/comments", postNewComment);
-app.delete("/api/comments/:comment_id", deleteComment);
-
-app.get("/api/users", getUsers);
+app.use("/api", apiRouter);
 
 app.all("*", badPath);
-
 app.use(customError);
 app.use(sqlError);
 
